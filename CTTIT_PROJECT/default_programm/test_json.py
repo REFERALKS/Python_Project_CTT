57404efe10cb6985088bb0d4832fa5743ce31208
Denis Kaporin <Dkap08630@gmail.com>
2025-11-27 16:25:41 +0300

    *

--- CTTIT_PROJECT/default_programm/test_json.py
+++ CTTIT_PROJECT/default_programm/test_json.py
@@ -1,12 +1,17 @@
 import json
+import os
 
-with open('D:\проекты\python_project\MY_Project_CTTIT\default_programm\test.json', 'r') as file:
+# Get the directory where the script is located
+base_path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_path, 'test.json')
+
+# Open the file using the absolute path
+with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 print(data)
 
-with open("test1.json", 'w') as file:
-    json.dump(data, file)
-
-data_as_json_string = json.dumps(data)
-data_again = json.loads(data_as_json_string)
+# Save to test1.json in the same folder
+output_path = os.path.join(base_path, 'test1.json')
+with open(output_path, 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
